0424485f29bcb4fac5a411fdfba270aeee5ce176
Ryan Magdaleno <ryamagdaleno@gmail.com>
2024-03-07 17:40:49 -0600

    00136 : : Single Number

---
+++ 00001-01000/00101-00200/00136-single-number.cpp
@@ -0,0 +1,17 @@
+/* easy :: bit-hack */
+/*
+    If you XOR a number with some k number twice,
+    you will get the original number back.
+    - -       - -
+    Time  :: O(n)
+    Space :: O(1)
+*/
+class Solution {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        int ret = 0;
+        for (const int& num : nums) { ret ^= num; }
+        return ret;
+    }
+};
