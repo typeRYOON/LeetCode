050afb0784d1e8a126960bb8c326fbd37b5c6855
Ryan Magdaleno <ryamagdaleno@gmail.com>
2024-03-07 17:34:55 -0600

    00083 : : Remove Duplicates from Sorted List

---
+++ 00001-01000/00001-00100/00083-remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,30 @@
+/* easy :: linked-list */
+/*
+    Check the cur->next->val and traverse
+    until there is a different number, the
+    first node should now connect to this
+    new node.
+    - -       - -
+    Time  :: O(n)
+    Space :: O(1)
+*/
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head)
+    {
+        if (!head) { return head; }
+        ListNode* cur = head;
+        ListNode* prev;
+
+        while (cur)
+        {
+            prev = cur;
+            while (cur && prev->val == cur->val) {
+                cur = cur->next;
+            }
+            prev->next = cur;
+        }
+        prev->next = nullptr;
+        return head;
+    }
+};
