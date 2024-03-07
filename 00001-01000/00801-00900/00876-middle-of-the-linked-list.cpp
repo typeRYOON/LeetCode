5d1126d8aaf1d8a628adf0b851fb27df03e47a73
Ryan Magdaleno <ryamagdaleno@gmail.com>
2024-03-07 17:45:41 -0600

    00876 : : Middle of the Linked List

---
+++ 00001-01000/00801-00900/00876-middle-of-the-linked-list.cpp
@@ -0,0 +1,24 @@
+/* easy :: linked-list */
+/*
+    Use a fast and slow node traverser.
+    fast traverses two nodes at a time
+    while slow traverses one. The slow
+    node will be in the middle at the
+    end of the traversal.
+    - -       - -
+    Time  :: O(n)
+    Space :: O(1)
+*/
+class Solution {
+public
+    ListNode middleNode(ListNode fast)
+    {
+        ListNode slow = fast;
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
