f29fc39dbda1bf645462cb400dc503626411a48b
Ryan Magdaleno <ryamagdaleno@gmail.com>
2024-03-07 19:45:02 -0600

    00191 : : Number of 1 Bits

---
+++ 00001-01000/00101-00200/00191-number-of-1-bits.cpp
@@ -0,0 +1,20 @@
+/* number-of-1-bits */
+/*
+    AND the first bit and SHIFT n right 1.
+    - -
+    Time  :: O(1) (always a uint32_t)
+    Space :: O(1)
+*/
+class Solution {
+public:
+    int hammingWeight(uint32_t n)
+    {
+        uint8_t c = 0;
+        while (n)
+        {
+            if (n & 0x1) { c++; }
+            n >>= 1;
+        }
+        return c;
+    }
+};
