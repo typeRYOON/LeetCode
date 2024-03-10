18c6ff4a6fa2419532ae02244a97fe5f5b458e8e
Ryan <ryamagdaleno@gmail.com>
2024-03-09 20:23:23 -0600

    01929 : : Concatenation Of Array

---
+++ 01001-02000/01901-02000/01929-concatenation-of-array.cpp
@@ -0,0 +1,19 @@
+/* easy :: array */
+/*
+    Resize by 2 and concat array.
+    - -       - -
+    Time  :: O(n)
+    Space :: O(n)
+*/
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums)
+    {
+        int n = (int)nums.size();
+        nums.resize(2*n);
+        for (int i = 0; i < n; ++i) {
+            nums[i+n] = nums[i];
+        }
+        return nums;
+    }
+};
